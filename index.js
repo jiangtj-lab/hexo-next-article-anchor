22bfa1e450836aca65af49c99049904ac13ebe6b
jiangtj <116749895@qq.com>
2019-12-24 14:12:28 +0800

    Change to marked:renderer filter

--- index.js
+++ index.js
@@ -3,26 +3,20 @@
 'use strict';
 
 const path = require("path");
+const cheerio = require("cheerio");
 
-hexo.extend.filter.register('after_post_render', function (data) {
-
-  var cheerio;
-  if (!cheerio) cheerio = require('cheerio');
-  var $ = cheerio.load(data.content, { decodeEntities: false });
-
-  var headings = $('h1, h2, h3, h4, h5, h6');
-
-  if (!headings.length) return;
-
-  headings.each(function() {
-    var id = $(this).attr('id');
-    $(this)
+hexo.extend.filter.register('marked:renderer', function(renderer) {
+  let originalRender = new renderer.constructor();
+  renderer.heading = (text, level) => {
+    let content = originalRender.heading(text, level);
+    let $ = cheerio.load(content, { decodeEntities: false });
+    let el = $(`h${level}`);
+    var id = el.attr('id');
+    el
       .addClass('article-heading')
       .append('<a class="article-anchor" href="#' + id + '" aria-hidden="true"></a>');
-  });
-  
-  data.content = $.html();
-
+    return $.html();
+  };
 });
 
 hexo.extend.filter.register('theme_inject', function(injects) {
