a32b1cab360ea3cdbce44236d749c035ea4c61e5
jiangtj <116749895@qq.com>
2020-05-26 09:42:29 +0800

    fix

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const cheerio = require("cheerio");
 const injector = require("hexo-extend-injector2")(hexo);
 
 hexo.extend.filter.register('marked:renderer', function(renderer) {
-  let originalRender = new renderer.constructor();
-  renderer.heading = (text, level) => {
-    let content = originalRender.heading(text, level);
+  let originalRender = renderer.heading;
+  renderer.heading = (...args) => {
+    let level = args[1];
+    let content = originalRender.apply(renderer, args);
     let $ = cheerio.load(content, { decodeEntities: false });
     let el = $(`h${level}`);
     var id = el.attr('id');
